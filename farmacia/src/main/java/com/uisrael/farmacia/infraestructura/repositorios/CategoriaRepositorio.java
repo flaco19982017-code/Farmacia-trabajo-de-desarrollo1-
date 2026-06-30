a86a02af41dd6ecdf297a3e413f7c4b232e9a227
flaco19982017-code <flaco19982017@gmail.com>
2026-06-29 22:43:37 -0500

    Delete farmacia directory

--- farmacia/src/main/java/com/uisrael/farmacia/infraestructura/repositorios/CategoriaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.uisrael.farmacia.infraestructura.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.uisrael.farmacia.infraestructura.persistencia.jpa.CategoriaEntity;
-
-public interface CategoriaRepositorio extends  JpaRepository<CategoriaEntity, Integer>{
-
-}
